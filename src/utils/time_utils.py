0757b31d8982d7b72a1b10fe0d8adcbe9434a3d9
Geismark <geismark@btinternet.com>
2024-05-20 16:20:42 +0100

    added /src/managers/

--- src/utils/time_utils.py
+++ src/utils/time_utils.py
@@ -4,5 +4,10 @@ def seconds_to_hour_time(seconds: int):
     return hrs, mins, secs
 
 
+def hour_time_to_seconds(hr: int, min: int, sec: int):
+    seconds = (hr * 60 * 60) + (min * 60) + sec
+    return seconds
+
+
 if __name__ == "__main__":
     print(seconds_to_hour_time(26468))
