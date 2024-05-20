cde129d304eeba783d8f3a1624eca97054323829
Geismark <geismark@btinternet.com>
2024-05-20 16:25:46 +0100

    added download_manager; updated download_utils.py

--- src/utils/download_utils.py
+++ src/utils/download_utils.py
@@ -2,20 +2,22 @@ from datetime import datetime
 import yt_dlp, os, pytube
 
 
-def download_video(url: str, start_time, duration=0.5):
-    currenttime = datetime.now()
-    timestring = currenttime.strftime("%H%M%S")
+def download_video(url: str, file_name, start_time, duration=0.5):
+    '''file_name does NOT include extensions
+    start_time is the timestamp to start the download in seconds
+    duration is duration of downloaded video, not interval of saved sections (recommend to leave this unchanged)'''
     end_time = start_time + duration
     yt_opts = {
-        # using the built in download_range_func causes yt-dlp to go through the entire video until getting to the section you are looking for
+        # using download_ranges or download_sections causes yt-dlp to go through the entire video until getting to the section you are looking for
         # "download_ranges": download_range_func(None, [(start_time, end_time)]),
+        # "download_sections": "*0:21:00 - 0:21:05",
         # INSTEAD, using ffmpeg's own options to only request the video from the start time
         # https://stackoverflow.com/a/76560401
         "external_downloader": "ffmpeg",
         "external_downloader_args": {"ffmpeg_i": ["-ss", str(start_time), "-to", str(end_time)]},
         # Issue: rarely (but sometimes) overwrites previously downloaded file with same name
         # actually desired in this case - no auto-deletion yet, but added download timestamp as a fix for now (different name -> always new file)
-        "outtmpl": f"{os.path.realpath("output")}/%(extractor)s-{timestring}-%(id)s-%(title)s.%(ext)s",
+        "outtmpl": f"{os.path.realpath("output")}/{file_name}.%(ext)s",
         # "format": "bestvideo[ext=mp4]",
         "verbose": True,
         "force_keyframes_at_cuts": True,
@@ -25,10 +27,10 @@ def download_video(url: str, start_time, duration=0.5):
     return
 
 def get_url_duration(url:str):
-    yt = pytube.Youtube(url)
+    yt = pytube.YouTube(url)
     return yt.length
 
 if __name__ == "__main__":
-    start_time = 180000
+    start_time = 16885
     download_video("https://www.youtube.com/watch?v=VUxwEfx7OPQ", start_time)
     # yt-dlp --list-formats https://www.youtube.com/watch?v=VUxwEfx7OPQ
